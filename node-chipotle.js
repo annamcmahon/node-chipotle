221c6675d3ee1747435ed3934a8c1563d39efab4
amcmaho4 <amcmaho4@nd.edu>
2016-11-28 21:34:30 -0500

    added the get possible order times functionality and submit order

--- node-chipotle.js
+++ node-chipotle.js
@@ -11,10 +11,10 @@ function Chipotle(options) {
   this._password = options.password;
   this._locationId = options.locationId;
   this._phoneNumber = options.phoneNumber;
-  this._currentOrderId = 0;
+  this._currentOrderId = undefined;
   this._cookie='';
   this._cookieStore = request.jar();
-  this.request = Promise.promisifyAll(request.defaults({jar: this._cookieStore, followRedirect: false}));
+  this.request = Promise.promisifyAll(request.defaults({jar: this._cookieStore, followRedirect: false, multiArgs: true}));
 };
 
 /*
@@ -28,19 +28,18 @@ Chipotle.prototype.login = function() {
     json:true,
     body: authPayload,
   })
+  .then(bodyOrError)
   .then(function(response){
-    console.log(response.body);
-    if(response.body.Status =="Success") {
-      console.log('login success mwahaha!');
-      this._cookie = 'st='+response.body.CustomerToken;
-      console.log(response.body.CustomerToken);
+      console.log('login success!');
+      // save the CustomerToken to be used in future requests
+      this._cookie = 'st='+response.CustomerToken;
       return;
-    } else {
-      throw new Error('login failed. check credentials.');
-    }
   });
 };
 
+/*
+  Adds an order
+*/
 Chipotle.prototype.addOrder = function(order) {
   var self = this;
   console.log("attempting to add order");
@@ -50,68 +49,129 @@ Chipotle.prototype.addOrder = function(order) {
     json:true,
     body: orderPayload
   })
+  .then(bodyOrError)
   .then(function(response){
-    console.log(response);
-    self._currentOrderId = response.body.OrderId;
+    self._currentOrderId = response.OrderId;
   });
 };
 
-Chipotle.prototype.getCurrentOrder = function(id) {
+/*
+  Gets the users current order
+*/
+Chipotle.prototype.getCurrentOrder = function() {
   var self = this;
-  console.log("getting the current order: "+ BASE_URL+ '/api/order/'+ self._currentOrderId + '/bag/' + self._locationId);
+  if(self._currentOrderId === undefined){
+    throw new Error("Current Customer Order Id is not defined");
+  }
   return self.request.getAsync({
     uri: BASE_URL+'/api/order/'+self._currentOrderId+'/bag/'+self._locationId,
     json:true,
   })
-  .then(function(body){
-    console.log("body of current order: ");
-    console.log(body.body.Meals);
+  .then(bodyOrError)
+  .then(function(response){
+    return response.Meals;
   });
 };
+
+Chipotle.prototype.getPossibleOrderTimes = function(){
+  var self = this;
+  // makes a blank order to the location to then get order times
+  var order = {};
+  return self.addOrder(order)
+  .then(function(){
+    return self.request.getAsync({
+      uri: BASE_URL+'/api/restaurant/'+self._locationId+'/pickuptimes/'+self._currentOrderId,
+      json:true,
+    })
+    .then(bodyOrError)
+    .then(function(response){
+      return response;
+    });
+  });
+}
+
+/*
+  Gets the menu for a specified location w/ prices
+*/
 Chipotle.prototype.getMenu = function(){
   var self = this;
   return self.request.getAsync({
     uri: BASE_URL+ '/api/restaurant/'+self._locationId+'/menu',
     json:true
-  }).then(function(body){
-    console.log("Menu: ");
-    console.log(body);
+  })
+  .then(bodyOrError)
+  .then(function(response){
+    return response;
   });
 }
+
+/*
+  Gets the users saved credit cards and returns the token of the first saved card,
+  token can then be used in the submit order request
+*/
 Chipotle.prototype.getSavedCreditCards = function(){
   var self = this;
   return self.request.getAsync({
     uri: BASE_URL+'/api/customer/creditcard',
     json:true,
     headers: {'Cookie': self._cookie}
-  }).then(function(response){
-    console.log("saved cards:");
-    console.log(response.body.WalletTokens);
+  })
+  .then(bodyOrError)
+  .then(function(response){
+    return response.WalletTokens[0]; // gets the info for the first credit card
   });
 }
 
-Chipotle.prototype.submitCurrentOrder = function(paymentInfo){
+/*
+  Set the order pickup time of the current order. This must be done before order can be submitted
+*/
+Chipotle.prototype.setOrderPickupTime = function(time){
   var self = this;
-  return self.request.getAsync({
-    uri: BASE_URL + '/api/order/'+self._currentOrderId+'/submit',
-    json: true,
-    body: paymentInfo
-  }).then(function(body){
-    if(response.body.Status =="Success") {
-      console.log('order placed');
-      return;
-    } else {
-      throw new Error('submit order request failed: '+ response.body.statusCode + ' body: ' + body);
-    }
+  var timePayload = time;
+  return self.request.postAsync({
+    uri: BASE_URL+ '/api/order/'+self._currentOrderId+'/pickuptime',
+    json:true,
+    body: timePayload,
+    headers:{'Cookie':self._cookie}
   })
+  .then(bodyOrError)
+  .then(function(response){
+    return;
+  });
 }
 
-function bodyOrError(response, body) {
+/*
+  Submits the current order
+*/
+Chipotle.prototype.submitCurrentOrder = function(){
+  var self = this;
+  var paymentInfo = {"paymentType":"credit","cardId":null, "storeId":self._locationId};
+  return self.getSavedCreditCards().then(function(cardDetails){
+    paymentInfo["paymentCard"]=cardDetails;
+	  paymentInfo["cardId"]= cardDetails["tokenId"] || null;
+    console.log(JSON.stringify(paymentInfo, null, 4));
+    self.request.postAsync({
+      uri: BASE_URL + '/api/order/'+self._currentOrderId+'/submit',
+      json: true,
+      body: paymentInfo
+    })
+    .then(bodyOrError)
+    .then(function(response){
+      return;
+    });
+  })
+}
+/*
+  Simple helper to status check the response and throw an error if it was not a 200.
+  If there was no error, returns the response body.
+*/
+function bodyOrError(response) {
   if(response.statusCode == 200) {
-    return body;
+    return response.body;
   } else {
-    throw new Error('request failed: '+ response.statusCode + ' body: ' + body);
+    throw new Error('Request Failed: '+ response.statusCode +
+          ' Message: ' + response.body.Message +
+          ' Error Type: ' + response.body.Type);
   }
 };
-
 module.exports = Chipotle;
--- test-chipotle.js
+++ test-chipotle.js
@@ -1,34 +1,32 @@
 var Chipotle = require('./node-chipotle.js');
+var config = require('./config.json');
 
 var chipotle = new Chipotle({
-    "username":"***",
-    "password":"***",
-    "persist":false, 
-    locationId: '***', 
-    phoneNumber:'***'});
-
-var order ={
-    "entree":{"type":"Burrito"},
-    "fillings":[{"Type":"Chicken"}],
-    "rice":[{"Type":"RiceWhite"}],
-    "beans":[{"Type":"BeansBlack"}],
-    "toppings":[{"Type":"GuacTopping"}],
-    "sides":[{"Type":"Chips"}],
-    "drinks":[],
-    "mealName":"Meal 5"};
+    "username": config["username"],
+    "password": config["password"],
+    "persist": config["persist"],
+    "locationId": config["locationId"],
+    "phoneNumber": config["phoneNumber"]
+  });
+var order ={"entree":{"type":"SidesDrinks"},"sides":[{"Type":"Chips","Name":"Chips","SummaryName":"Chips","Price":1.35,"selected":true,"state":"number","quantity":1}],"drinks":[],"mealName":"Meal 1"};
+var time = {
+  "SlotEndTime":"2016-11-28T10:00:00",
+  "SlotStartTime":"2016-11-28T9:45:00"
+};
 
 chipotle.login().then(function() {
   console.log("logged in! ");
   chipotle.getSavedCreditCards().then(function(body){
-    console.log("got cards");
-  });
-  chipotle.getMenu().then(function(){
-    console.log("got the menu");
-  });
-  chipotle.addOrder(order).then(function(){
+    console.log("got cards: ");
+    console.log(body);
+    chipotle.addOrder(order).then(function(){
       console.log("order added");
-      chipotle.getCurrentOrder().then(function(){
-        console.log("got the current order");
+      chipotle.setOrderPickupTime(time).then(function(){
+        console.log("set the order pickup time");
+        chipotle.submitCurrentOrder().then(function(){
+         console.log("submitted the current order");
+        });
       });
+    });
   });
 });
